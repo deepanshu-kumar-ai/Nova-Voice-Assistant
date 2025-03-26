855c8553c645d1370f889660d448b4de23089dcc
Deepanshu kumar <deepanshuvadav@gmail.com>
2025-03-26 20:10:49 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+# musicLibrary.py
+music = {
+    "Believer": "https://www.youtube.com/watch?v=W0DM5lcj6mw&ab_channel=7clouds",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "shape  of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8&ab_channel=EdSheeran"
+}
